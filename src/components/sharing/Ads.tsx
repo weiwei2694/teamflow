fd04ad8f4d50ae17ba8718bd4d5ff78d830a0098
Arya Priema <99aryaprima@gmail.com>
2023-09-17 12:55:53 +0700

    feat: Ads

--- src/components/sharing/Ads.tsx
+++ src/components/sharing/Ads.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { ads } from '@/constants'
+import Line from '../sub-component/Line'
+import Image from 'next/image'
 
 const Ads = () => {
   return (
-    <div>Ads</div>
+    <div className="section__padding py-10 lg:py-20">
+      <div className="flex flex-col md:flex-row justify-between">
+        <div>
+          <Line color="bg-yellow-100" />
+          <h4 className="mt-5 text-dark-300 text-xl sm:text-2xl md:text-3xl font-bold tracking-[2%]">
+            See why over 100,000 teams choose TeamFlow.com
+          </h4>
+        </div>
+        <div className="w-full overflow-x-auto">
+          <ul className="flex justify-start lg:justify-end min-w-[700px] gap-x-8 whitespace-nowrap">
+            {ads.map(ad => (
+              <Image src={ad.src} alt={ad.alt} width={ad.width} height={ad.height} className="object-contain" />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,18 +1,45 @@
 export const routes = [
-    {
-        title: "Product",
-        href: "/"
-    },
-    {
-        title: "Solution",
-        href: "/"
-    },
-    {
-        title: "Enterprise",
-        href: "/"
-    },
-    {
-        title: "Pricing",
-        href: "/"
-    }
-]
+	{
+		title: "Product",
+		href: "/",
+	},
+	{
+		title: "Solution",
+		href: "/",
+	},
+	{
+		title: "Enterprise",
+		href: "/",
+	},
+	{
+		title: "Pricing",
+		href: "/",
+	},
+];
+
+export const ads = [
+	{
+		src: "/assets/hulu.png",
+		alt: "Hulu",
+        width: 114,
+        height: 93
+	},
+	{
+		src: "/assets/bbc-studios.png",
+		alt: "Bbc Studios",
+        width: 158,
+        height: 108
+	},
+	{
+		src: "/assets/universal.png",
+		alt: "Universal",
+        width: 128,
+        height: 95
+	},
+	{
+		src: "/assets/adobe.png",
+		alt: "Adobe",
+        width: 137,
+        height: 95
+	},
+];
